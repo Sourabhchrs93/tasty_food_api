21d8aece03d2460b6ac8dfef8cac97aaaf1c5370
Sourabh Chourasia <sourabh.chourasia@noodle.ai>
2019-06-26 10:49:48 +0530

    defined class object for read docs from txt file

--- src/services/random_sample.py
+++ src/services/random_sample.py
@@ -1,31 +1,41 @@
 import random
 
 
-def random_sampler(filename, k):
-    sample_list = []
-    with open(filename, 'rb') as f:
-        f.seek(0, 2)
-        filesize = f.tell()
-        random_set = sorted(random.sample(range(filesize), k))
+class Sample:
+    def __init__(self, filename):
+        self.sample_list = []
+        self.filename = filename
 
-        for i in range(k):
-            f.seek(random_set[i])
-            # Skip current line (because we might be in the middle of a line.
-            f.readline()
-            # Append the next line to the sample set
-            new_line = str(f.readline())
-            while 'product/productId:' not in new_line:
+    def random_sampler(self, k):
+        with open(self.filename, 'rb') as f:
+            f.seek(0, 2)
+            filesize = f.tell()
+            random_set = sorted(random.sample(range(filesize), k))
+
+            for i in range(k):
+                f.seek(random_set[i])
+                # Skip current line (because we might be in the middle of a line.
+                f.readline()
+                # Append the next line to the sample set
                 new_line = str(f.readline())
-            doc = dict()
-            doc['product/productId'] = new_line.split("product/productId: ", 1)[-1][:-3]
-            doc['review/userId'] = str(f.readline()).split("review/userId: ", 1)[-1][:-3]
-            doc['review/profileName'] = str(f.readline()).split("review/profileName: ", 1)[-1][:-3]
-            doc['review/helpfulness:'] = str(f.readline()).split("review/helpfulness: ", 1)[-1][:-3]
-            doc['review/score'] = str(f.readline()).split("review/score: ", 1)[-1][:-3]
-            doc['review/time'] = str(f.readline()).split("review/time: ", 1)[-1][:-3]
-            doc['review/summary'] = str(f.readline()).split("review/summary: ", 1)[-1][:-3]
-            doc['review/text'] = str(f.readline()).split("review/text: ", 1)[-1][:-3]
+                while 'product/productId:' not in new_line:
+                    new_line = str(f.readline())
+                doc = dict()
+                doc['product/productId'] = new_line.split("product/productId: ", 1)[-1][:-3]
+                doc['review/userId'] = str(f.readline()).split("review/userId: ", 1)[-1][:-3]
+                doc['review/profileName'] = str(f.readline()).split("review/profileName: ", 1)[-1][:-3]
+                doc['review/helpfulness:'] = str(f.readline()).split("review/helpfulness: ", 1)[-1][:-3]
+                doc['review/score'] = str(f.readline()).split("review/score: ", 1)[-1][:-3]
+                doc['review/time'] = str(f.readline()).split("review/time: ", 1)[-1][:-3]
+                doc['review/summary'] = str(f.readline()).split("review/summary: ", 1)[-1][:-3]
+                doc['review/text'] = str(f.readline()).split("review/text: ", 1)[-1][:-3]
 
-            sample_list.append(doc)
+                self.sample_list.append(doc)
 
-    return sample_list
+        return len(self.sample_list)
+
+    def get_sample(self):
+        return self.sample_list
+
+
+sample = Sample('finefoods.txt')
