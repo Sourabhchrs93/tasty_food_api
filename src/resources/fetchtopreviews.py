6468820085eb3b03f395cb35ccd55b6cd13bce2f
Sourabh Chourasia <sourabh.chourasia@noodle.ai>
2019-06-30 13:46:26 +0530

    removed commented code

--- src/resources/fetchtopreviews.py
+++ src/resources/fetchtopreviews.py
@@ -8,51 +8,6 @@ import json
 class FetchTopReviews(Resource):
     def __init__(self):
         pass
-    '''
-    def post(self):
-        queries = json.loads(request.data.decode('utf-8'))['data']
-
-        query_length = len(queries)
-
-        k = int(request.args.get("k", 20))
-        sample_reviews = sample.get_sample()
-        top_k = []
-        for index, review in enumerate(sample_reviews):
-            # convert query words into lowercase and put it into a dict()
-            query_dict = dict()
-            for i, query in enumerate(queries):
-                query_dict[query.lower()] = 1
-
-            review_index = 0
-            length = len(review['review/text'])
-            word = ''
-            score = 0
-            while review_index < length:
-                # search in list of special chars. we might get something like 'good.'
-                if review['review/text'][review_index] not in ['`','~','!','@','#','$','%','^','&','*','(',')','_','-',
-                                                               '+','=','{','[','}','}','|','\\',':',';','"',"'",'<',','
-                                                               ,'>','.','?','/', ' ']:
-                    word += review['review/text'][review_index]
-                else:
-                    word = word.lower()
-                    if word in query_dict:
-                        del query_dict[word]
-                        score += 1
-                        if score == query_length:   # break the wile loop if all the words are found
-                            break
-                    word = ''
-                review_index += 1
-
-            top_k = self.insert_ele(top_k, k, (index, score/query_length))
-
-        print(top_k)
-        output = []
-        for ki in top_k:
-            del sample_reviews[ki[0]]['review/text1']
-            output.append(sample_reviews[ki[0]])
-
-        return output
-    '''
 
     def post(self):
         queries = json.loads(request.data.decode('utf-8'))['data']
