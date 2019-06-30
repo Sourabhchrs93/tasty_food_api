b7f92f604f579ef432b66bc414d06d8f5987b71e
Sourabh Chourasia <sourabh.chourasia@noodle.ai>
2019-06-30 13:45:36 +0530

    calculate score using set datatype

--- src/resources/fetchtopreviews.py
+++ src/resources/fetchtopreviews.py
@@ -8,7 +8,7 @@ import json
 class FetchTopReviews(Resource):
     def __init__(self):
         pass
-
+    '''
     def post(self):
         queries = json.loads(request.data.decode('utf-8'))['data']
 
@@ -48,7 +48,46 @@ class FetchTopReviews(Resource):
         print(top_k)
         output = []
         for ki in top_k:
+            del sample_reviews[ki[0]]['review/text1']
             output.append(sample_reviews[ki[0]])
+
+        return output
+    '''
+
+    def post(self):
+        queries = json.loads(request.data.decode('utf-8'))['data']
+
+        query_length = len(queries)
+
+        k = int(request.args.get("k", 20))
+        sample_reviews = sample.get_sample()
+        top_k = []
+        queries_set = set()
+        for word in queries:
+            queries_set.add(word.lower())
+        for index, review in enumerate(sample_reviews):
+            review_set = review['review/text1']
+
+            diff_set = queries_set - review_set
+            score = query_length - len(diff_set)
+
+            top_k = self.insert_ele(top_k, k, (index, score/query_length))
+
+        print(top_k)
+        output = []
+        for ki in top_k:
+            out_doc = dict()
+            out_doc['_id'] = sample_reviews[ki[0]]['_id']
+            out_doc['product/productId'] = sample_reviews[ki[0]]['product/productId']
+            out_doc['review/userId'] = sample_reviews[ki[0]]['review/userId']
+            out_doc['review/profileName'] = sample_reviews[ki[0]]['review/profileName']
+            out_doc['review/helpfulness'] = sample_reviews[ki[0]]['review/helpfulness']
+            out_doc['review/score'] = sample_reviews[ki[0]]['review/score']
+            out_doc['review/time'] = sample_reviews[ki[0]]['review/time']
+            out_doc['review/summary'] = sample_reviews[ki[0]]['review/summary']
+            out_doc['review/text'] = sample_reviews[ki[0]]['review/text']
+
+            output.append(out_doc)
 
         return output
 
