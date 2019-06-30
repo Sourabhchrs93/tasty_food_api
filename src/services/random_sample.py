0967bf1821931d4f553833e4d779bcbd1d287969
Sourabh Chourasia <sourabh.chourasia@noodle.ai>
2019-06-30 13:39:14 +0530

    maintaining set for review/sumary an review/text

--- src/services/random_sample.py
+++ src/services/random_sample.py
@@ -10,8 +10,8 @@ class Sample:
         self.sample_list = []
         count = 0
         with open(self.filename, 'rb') as f:
-            new_line = str(f.readline())
             while count < k:
+                new_line = str(f.readline())
                 while 'product/productId:' not in new_line:
                     new_line = str(f.readline())
                 doc = dict()
@@ -24,6 +24,24 @@ class Sample:
                 doc['review/time'] = str(f.readline()).split("review/time: ", 1)[-1][:-3]
                 doc['review/summary'] = str(f.readline()).split("review/summary: ", 1)[-1][:-3]
                 doc['review/text'] = str(f.readline()).split("review/text: ", 1)[-1][:-3]
+                doc['review/text1'] = set()
+                review = doc['review/summary'] + " "+ doc['review/text']
+
+                review_length = len(review)
+                review_index = 0
+                word = ''
+                while review_index < review_length:
+                    # search in list of special chars. we might get something like 'good.'
+                    if review[review_index] not in ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+                                                    '_', '-', '+', '=', '{', '[', '}', '}', '|', '\\', ':', ';',
+                                                    '"', "'", '<', ',', '>', '.', '?', '/', ' ']:
+                        word += review[review_index]
+                    else:
+                        word = word.lower()
+                        doc['review/text1'].add(word)
+                        word = ''
+                    review_index += 1
+
                 self.sample_list.append(doc)
                 count += 1
         return len(self.sample_list)
